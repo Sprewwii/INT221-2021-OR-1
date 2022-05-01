d6bdb0df2b8198643b71e3d4a2dad5a5ac2c8384
Sprewwii <prewprewwy@gmail.com>
2022-05-01 20:55:13 +0700

    All: [Edit] remove icon + add no schedule

--- src/components/BaseScheduleList.vue
+++ src/components/BaseScheduleList.vue
@@ -42,7 +42,8 @@ const bookingSort = computed(() =>
             </th>
           </tr>
         </thead>
-        <tbody v-if="bookingList.length === 0"></tbody>
+        <tbody v-if="bookingList.length === 0">
+        <td colspan="6" class="py-64 text-xl">No Scheduled Event</td></tbody>
         <tbody v-else v-for="booking in bookingSort" :key="booking.id">
           <tr
             class="border-t bg-gray-800 border-gray-700 text-gray-400 hover:bg-gray-600 text-center font-normal bg-opacity-50 sm:text-sm"
