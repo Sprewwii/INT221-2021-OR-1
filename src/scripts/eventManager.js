a418293aec831ce2ce003ef5568c66ffc694450b
Sprewwii <prewprewwy@gmail.com>
2022-05-02 00:27:49 +0700

    All: [Edit]

--- src/scripts/eventManager.js
+++ src/scripts/eventManager.js
@@ -2,12 +2,12 @@ import {reactive} from 'vue'
 export const eventManager = reactive({
   eventList: [],
   getEvents: async function () {
-    const res = await fetch("http://172.17.0.3:8080/api/booking") 
+    const res = await fetch("http://ip21or1.sit.kmutt.ac.th:8080/api/booking") 
     if (res.status === 200) {
       this.eventList = await res.json()
       console.log(this.eventList)
     } else {
-      console.log("ไม่มีข้อมูลอีเว้นนนนน")
+      console.log("บ่มีจ้า")
     }
   },
   test:[]
