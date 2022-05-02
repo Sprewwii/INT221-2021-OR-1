e23695c00c507c548f51bcb02a04fa9e56138a2a
Sprewwii <prewprewwy@gmail.com>
2022-05-03 01:29:02 +0700

    Mos: [edit] object + path

--- src/scripts/eventManager.js
+++ src/scripts/eventManager.js
@@ -2,7 +2,7 @@ import {reactive} from 'vue'
 export const eventManager = reactive({
   eventList: [],
   getEvents: async function () {
-    const res = await fetch("http://10.4.56.94:8080/api/booking") 
+    const res = await fetch("http://10.4.56.94:8080/api/events") 
     if (res.status === 200) {
       this.eventList = await res.json()
       console.log(this.eventList)
