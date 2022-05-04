bd9dfb60bc6d81cb812b41d085ca895c028a91f3
AthitiJames <69097916+AthitiJames@users.noreply.github.com>
2022-05-04 23:01:52 +0700

    add proxy Server

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api':{
+        target: 'http://172.17.0.3:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
 })
