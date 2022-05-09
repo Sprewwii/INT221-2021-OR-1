3f7131f3ccdfb5942aaf3a4a304496b403e34e00
thanadol <mosqto2001@gmail.com>
2022-05-10 02:55:58 +0700

    Mos: [Edit] ui 5

--- src/components/BasePopupCreate.vue
+++ src/components/BasePopupCreate.vue
@@ -97,7 +97,7 @@ const addingEvent = ref({})
 
 
 
-                        <button @click="$emit('createEvent', addingEvent, $event)"
+                        <button @click="$emit('createEvent', addingEvent, $event);addingEvent = {}"
                             class="w-full text-white bg-pink-600 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800">Create</button>
 
 
