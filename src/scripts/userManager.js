1f51d9146c69847d4db6cca4e5b0e6606364e8a6
mos <mosqto2001@gmail.com>
2022-08-22 23:38:13 +0700

    All: [add] create user and edit user

--- src/scripts/userManager.js
+++ src/scripts/userManager.js
@@ -2,89 +2,86 @@ import { reactive } from "vue";
 import { roles } from "./roles.js";
 
 export const userManager = reactive({
-    userList: [],
-    getUsers: async function () {
-        const res = await fetch(`${import.meta.env.VITE_API}/api/users`);
-        if (res.status === 200) {
-          this.userList = await res.json();
-          this.userList.forEach((user) => {
-            user.role = roles[user.role]
-        
-          })
-        } else {
-          console.log("ไม่พบข้อมูล");
-        }
+  userList: [],
+  getUsers: async function () {
+    const res = await fetch(`${import.meta.env.VITE_API}/api/users`);
+    if (res.status === 200) {
+      this.userList = await res.json();
+      this.userList.forEach((user) => {
+        user.role = roles[user.role];
+      });
+    } else {
+      console.log("ไม่พบข้อมูล");
+    }
+  },
+  getUserById: async function (userId) {
+    const res = await fetch(`${import.meta.env.VITE_API}/api/users/${userId}`);
+    if (res.status === 200) {
+      let user = await res.json();
+      return { userId: userId, ...user };
+    } else {
+      console.log(`ไม่พบข้อมูล event Id: ${userId}`);
+    }
+  },
+  createUser: async function (user) {
+    const res = await fetch(`${import.meta.env.VITE_API}/api/users`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
       },
-      getUserById: async function (userId) {
-        const res = await fetch(`${import.meta.env.VITE_API}/api/users/${userId}`);
-        if (res.status === 200) {
-          let user = await res.json()
-          return {userId: userId, ...user};
-        } else {
-          console.log(`ไม่พบข้อมูล event Id: ${userId}`);
-        }
-      },
-      createUser: async function (user) {
-        const res = await fetch(`${import.meta.env.VITE_API}/api/users`, {
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify({
-            name: user.name,
-            email: user.email,
-            role: user.role,
-          }),
-        })
+      body: JSON.stringify({
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      })
+  })
+  .then(res => {
+    if(res.status === 200) {
+      this.getUsers();
+      console.log("create")
+     }
+  })
+  .catch(err => {
 
-        const info = await res.json()
-          if (res.status === 200){
-            this.getUsers();
-            // return true;
-          }
-      
+  })
 
-        // console.log(res.json)
-        
-      
-       
-      },
-      deleteUser: async function (userId) {
-        const res = await fetch(`${import.meta.env.VITE_API}/api/users/${userId}`, {
-          method: "DELETE",
-        })
+    // console.log(res.json)
+  },
+  deleteUser: async function (userId) {
+    const res = await fetch(`${import.meta.env.VITE_API}/api/users/${userId}`, {
+      method: "DELETE",
+    });
 
-        const info = await res.text()
-        if (res.status === 200) {
-          this.getUsers();
-          console.log(info);
-        } else {
-          console.log(`ไม่พบข้อมูล user Id: ${userId}`);
-        }
-      },
+    const info = await res.text();
+    if (res.status === 200) {
+      this.getUsers();
+      console.log(info);
+    } else {
+      console.log(`ไม่พบข้อมูล user Id: ${userId}`);
+    }
+  },
 
-      editUser: async function (user) {
-        console.log(user);
-        const res = await fetch(
-          `${import.meta.env.VITE_API}/api/users/${user.userId}`,
-          {
-            method: "PATCH",
-            headers: {
-              "content-type": "application/json",
-            },
-            body: JSON.stringify({
-              name: user.name,
-              email: user.email,
-              role: user.role
-            }),
-          }
-        );
-    
-        if (res.status === 200) {
-          this.getUsers();
-        } else {
-          console.log("ไม่สามารถแก้ไขข้อมูลได้");
-        }
-      },
-      
-})
+  editUser: async function (user) {
+    console.log(user);
+    const res = await fetch(
+      `${import.meta.env.VITE_API}/api/users/${user.userId}`,
+      {
+        method: "PATCH",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          name: user.name,
+          email: user.email,
+          role: user.role,
+        }),
+      }
+    );
+
+    if (res.status === 200) {
+      this.getUsers();
+    } else {
+      console.log("ไม่สามารถแก้ไขข้อมูลได้");
+    }
+  },
+});
