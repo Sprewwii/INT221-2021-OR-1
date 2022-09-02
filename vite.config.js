87db9d4b314165c7e29a381e1102529ab312f2a8
athitijames <athitijames@gmail.com>
2022-09-02 17:57:03 +0700

    update https

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'http://intproj21.sit.kmutt.ac.th/or1/'
+  base: 'http://ip21or1.sit.kmutt.ac.th/or1/'
 })
